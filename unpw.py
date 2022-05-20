03a2d7fd187f5917a8f0774432ba26c0c5dcd8b8
sachusal123 <105854333+sachusal123@users.noreply.github.com>
2022-05-20 14:47:37 +0530

    enter username& PW and recheck whether it is same

---
+++ unpw.py
@@ -0,0 +1,7 @@
+username=input("Enter Username:")
+password=input("Enter Password:")
+
+if username==password :
+    print("Username and password are same")
+else:
+    print("Invalid username or password")
